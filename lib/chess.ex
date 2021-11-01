7111c4c93dc0e2e427705aa58b648c5adb82362e
Rafael Mota <rma7@cin.ufpe.br>
2021-11-01 17:48:36 -0300

    Refactor check? method and format code

--- lib/chess.ex
+++ lib/chess.ex
@@ -24,30 +24,38 @@ defmodule Chess do
   end
 
   def valid_moves(game) do
-    moves = for x <- 0..7, y <- 0..7, z <- 0..7, w <- 0..7, valid_move?(game, {x, y}, {z, w}) do
-      {{x, y}, {z, w}}
-    end
-
-    Enum.reduce moves, %{}, fn ({origin, dest}, acc) ->
-      cond do
-        l = Map.get(acc, origin) -> Map.put acc, origin, [dest | l]
-        true -> Map.put acc, origin, [dest]
+    moves =
+      for x <- 0..7, y <- 0..7, z <- 0..7, w <- 0..7, valid_move?(game, {x, y}, {z, w}) do
+        {{x, y}, {z, w}}
       end
-    end
+
+    Enum.reduce(moves, %{}, fn {origin, dest}, acc ->
+      {_, new} =
+        Map.get_and_update(acc, origin, fn l ->
+          new_l =
+            case l do
+              nil -> [dest]
+              l -> [dest | l]
+            end
+
+          {l, new_l}
+        end)
+      new
+    end)
   end
 
   def check?(%{player: current_player} = game) do
     next_player_game = next_player(game)
     moves = valid_moves(next_player_game)
 
-    Enum.any? moves, fn({_k, v}) ->
-      Enum.any?(v, fn(x) ->
+    Enum.any?(moves, fn {_k, v} ->
+      Enum.any?(v, fn x ->
         case value_at(game, x) do
           {player, type} -> player == current_player && type == :king
           nil -> false
         end
       end)
-    end
+    end)
   end
 
   def valid_move?(%{player: current_player} = game, origin, dest) do
@@ -58,6 +66,7 @@ defmodule Chess do
             nil -> convert_position(dest)
             {dest_player, _type} -> dest_player != current_player
           end && current_player == player
+
         nil ->
           false
       end
@@ -75,6 +84,7 @@ defmodule Chess do
           :king -> valid_move_king?(game, origin, dest)
           _ -> true
         end
+
       true ->
         false
     end
@@ -84,7 +94,7 @@ defmodule Chess do
     dx = abs(x - z)
     dy = abs(y - w)
 
-    dx <= 1  && dy <= 1
+    dx <= 1 && dy <= 1
   end
 
   defp valid_move_queen?(game, origin, dest) do
@@ -94,49 +104,57 @@ defmodule Chess do
   defp valid_move_bishop?(game, {x, y}, {z, w}) do
     px = z - x
     py = w - y
+
     cond do
       abs(px) == abs(py) ->
-        {mx, my} = { (if px < 0, do: -1, else: 1), (if py < 0, do: -1, else: 1)}
+        {mx, my} = {if(px < 0, do: -1, else: 1), if(py < 0, do: -1, else: 1)}
 
         apx = abs(px)
-        apx == 1 || Enum.all? 1..apx-1, fn(a) ->
-          xa = x + a * mx
-          ya = y + a * my
 
-          value_at(game, {xa, ya}) == nil
-        end
-      true -> false
+        apx == 1 ||
+          Enum.all?(1..(apx - 1), fn a ->
+            xa = x + a * mx
+            ya = y + a * my
+
+            value_at(game, {xa, ya}) == nil
+          end)
+
+      true ->
+        false
     end
   end
 
   defp valid_move_horse?(_game, {x, y}, {z, w}) do
     ((x + 2 == z || x - 2 == z) && (y + 1 == w || y - 1 == w)) ||
-    ((y + 2 == w || y - 2 == w) && (x + 1 == z || x - 1 == z))
+      ((y + 2 == w || y - 2 == w) && (x + 1 == z || x - 1 == z))
   end
 
   defp valid_move_tower?(game, {x, y}, {z, w}) do
     cond do
-      x == z -> Enum.all? y..w, fn(a) -> a == y || a == w || value_at(game, {x, a}) == nil end
-      y == w -> Enum.all? x..z, fn(a) -> a == x || a == z || value_at(game, {a, y}) == nil end
+      x == z -> Enum.all?(y..w, fn a -> a == y || a == w || value_at(game, {x, a}) == nil end)
+      y == w -> Enum.all?(x..z, fn a -> a == x || a == z || value_at(game, {a, y}) == nil end)
       true -> false
     end
   end
 
   defp valid_move_pawn?(game, {x, y} = origin, {z, w}) do
-    {player, _type} = value_at(game,  origin)
+    {player, _type} = value_at(game, origin)
 
-    {direction, initial_row} = case player do
-      :black -> {1, 1}
-      :white -> {-1, 6}
-    end
+    {direction, initial_row} =
+      case player do
+        :black -> {1, 1}
+        :white -> {-1, 6}
+      end
 
     new_x = x + direction
-    ((new_x == z) && cond do
-      y == w -> true
-      y - 1 == w -> value_at(game, {new_x, y - 1})
-      y + 1 == w -> value_at(game, {new_x, y + 1})
-      true -> false
-    end) || (x == initial_row && x + direction * 2 == z && y == w)
+
+    (new_x == z &&
+       cond do
+         y == w -> true
+         y - 1 == w -> value_at(game, {new_x, y - 1})
+         y + 1 == w -> value_at(game, {new_x, y + 1})
+         true -> false
+       end) || (x == initial_row && x + direction * 2 == z && y == w)
   end
 
   def value_at(%{board: board}, pos) do
@@ -157,6 +175,7 @@ defmodule Chess do
           |> List.replace_at(pos_index, nil)
 
         %Game{game | board: new_board}
+
       true ->
         game
     end
--- test/chess_test.exs
+++ test/chess_test.exs
@@ -56,15 +56,15 @@ defmodule Chess.ChessTest do
   end
 
   test "valid_move? should return true if the move is valid - pawn - white" do
-    assert Chess.new_game |> Chess.valid_move?({6, 0}, {5, 0})
+    assert Chess.new_game() |> Chess.valid_move?({6, 0}, {5, 0})
   end
 
   test "valid_move? should return true if the move is valid - pawn - black" do
-    assert Chess.new_game |> Chess.next_player |> Chess.valid_move?({1, 0}, {2, 0})
+    assert Chess.new_game() |> Chess.next_player() |> Chess.valid_move?({1, 0}, {2, 0})
   end
 
   test "valid_move? should return false if the move is invalid - pawn - black" do
-    game = Chess.new_game |> Chess.next_player()
+    game = Chess.new_game() |> Chess.next_player()
 
     refute Chess.valid_move?(game, {1, 0}, {1, 1})
     refute Chess.valid_move?(game, {1, 0}, {2, 1})
@@ -72,7 +72,7 @@ defmodule Chess.ChessTest do
   end
 
   test "valid_move? should return false if the move is invalid pawn - white" do
-    game = Chess.new_game
+    game = Chess.new_game()
 
     refute Chess.valid_move?(game, {6, 0}, {3, 0})
     refute Chess.valid_move?(game, {6, 0}, {6, 1})
@@ -81,31 +81,32 @@ defmodule Chess.ChessTest do
   end
 
   test "valid_move? should return true if pawn moves are in the transversal and the move eats another players pieces - white" do
-    assert Chess.new_game
-    |> Chess.move({1, 0}, {5, 1})
-    |> Chess.valid_move?({6, 0}, {5, 1})
+    assert Chess.new_game()
+           |> Chess.move({1, 0}, {5, 1})
+           |> Chess.valid_move?({6, 0}, {5, 1})
   end
 
   test "valid_move? should return true if pawn moves are in the transversal and the move eats another players pieces - black" do
-    assert Chess.new_game
-    |> Chess.move({6, 0}, {2, 1})
-    |> Chess.next_player()
-    |> Chess.valid_move?({1, 0}, {2, 1})
+    assert Chess.new_game()
+           |> Chess.move({6, 0}, {2, 1})
+           |> Chess.next_player()
+           |> Chess.valid_move?({1, 0}, {2, 1})
   end
 
   test "valid_move? should return true if pawn moves two steps over if they are in their initial position" do
     assert Chess.new_game()
-    |> Chess.valid_move?({6, 1}, {4, 1})
+           |> Chess.valid_move?({6, 1}, {4, 1})
   end
 
   test "valid_move? should return false if pawn moves two steps outside the initial position" do
     refute Chess.new_game()
-    |> Chess.move({6, 1}, {5, 1})
-    |> Chess.valid_move?({5, 1}, {3, 1})
+           |> Chess.move({6, 1}, {5, 1})
+           |> Chess.valid_move?({5, 1}, {3, 1})
   end
 
   test "valid_move? should return true if the tower moves in vertical lines" do
-    game = Chess.new_game
+    game =
+      Chess.new_game()
       |> Chess.move({6, 0}, {6, 1})
 
     assert Chess.valid_move?(game, {7, 0}, {6, 0})
@@ -115,7 +116,8 @@ defmodule Chess.ChessTest do
   end
 
   test "valid_move? should return true if the tower moves in horizontal lines" do
-    game = Chess.new_game
+    game =
+      Chess.new_game()
       |> Chess.move({7, 0}, {5, 0})
 
     assert Chess.valid_move?(game, {5, 0}, {5, 1})
@@ -123,7 +125,8 @@ defmodule Chess.ChessTest do
   end
 
   test "valid_move? should return false if the tower moves in tilted lines" do
-    game = Chess.new_game
+    game =
+      Chess.new_game()
       |> Chess.move({7, 0}, {5, 0})
 
     refute Chess.valid_move?(game, {5, 0}, {6, 1})
@@ -131,28 +134,29 @@ defmodule Chess.ChessTest do
   end
 
   test "valid_move? should return false if tower tries to move over other piece" do
-    game = Chess.new_game
+    game = Chess.new_game()
 
     refute Chess.valid_move?(game, {7, 0}, {4, 0})
     refute Chess.valid_move?(game, {7, 7}, {5, 7})
   end
 
   test "valid_move? should return false if horse tries to move to invalid spaces" do
-    game = Chess.new_game
+    game = Chess.new_game()
 
     refute Chess.valid_move?(game, {7, 1}, {5, 1})
     refute Chess.valid_move?(game, {7, 1}, {4, 1})
   end
 
   test "valid_move? should return true if the horse do a valid move vertical" do
-    game = Chess.new_game
+    game = Chess.new_game()
 
-    assert Chess.valid_move?(game, {7,1}, {5,0})
-    assert Chess.valid_move?(game, {7,1}, {5,2})
+    assert Chess.valid_move?(game, {7, 1}, {5, 0})
+    assert Chess.valid_move?(game, {7, 1}, {5, 2})
   end
 
   test "valid_move? should return true if the horse do a valid move horizonal" do
-    game = Chess.new_game
+    game =
+      Chess.new_game()
       |> Chess.move({7, 1}, {4, 2})
 
     assert Chess.valid_move?(game, {4, 2}, {3, 0})
@@ -162,27 +166,27 @@ defmodule Chess.ChessTest do
   end
 
   test "valid_move? should return false if try to move a piece that is not from the current player" do
-    refute Chess.new_game |> Chess.valid_move?({1, 0}, {2, 0})
+    refute Chess.new_game() |> Chess.valid_move?({1, 0}, {2, 0})
   end
 
   test "valid_move? should return false if try to move a piece that doesnt exist" do
-    refute Chess.new_game |> Chess.valid_move?({2, 0}, {3, 0})
+    refute Chess.new_game() |> Chess.valid_move?({2, 0}, {3, 0})
   end
 
   test "valid_move? should return false if try to move to a dest that already has a piece for the same team" do
-    refute Chess.new_game |> Chess.move({6, 1},{5, 0}) |> Chess.valid_move?({6, 0}, {5, 0})
+    refute Chess.new_game() |> Chess.move({6, 1}, {5, 0}) |> Chess.valid_move?({6, 0}, {5, 0})
   end
 
   test "valid_move? should return false if try to move to invalid position" do
-    refute Chess.new_game |> Chess.valid_move?({6, 0}, {-1, 0})
+    refute Chess.new_game() |> Chess.valid_move?({6, 0}, {-1, 0})
   end
 
   test "valid_move? should return false if try to move from invalid position" do
-    refute Chess.new_game |> Chess.valid_move?({-1, 0}, {1, 0})
+    refute Chess.new_game() |> Chess.valid_move?({-1, 0}, {1, 0})
   end
 
   test "valid_move? should return false when try to move to the same position" do
-    game = Chess.new_game
+    game = Chess.new_game()
 
     refute Chess.valid_move?(game, {6, 0}, {6, 0})
     refute Chess.valid_move?(game, {7, 0}, {7, 0})
@@ -190,17 +194,19 @@ defmodule Chess.ChessTest do
   end
 
   test "valid_move? should return false when try to move bishop horinzontaly or verically" do
-    game = Chess.new_game
+    game =
+      Chess.new_game()
       |> Chess.move({7, 2}, {4, 1})
 
     refute Chess.valid_move?(game, {4, 1}, {3, 1})
     refute Chess.valid_move?(game, {4, 1}, {5, 1})
     refute Chess.valid_move?(game, {4, 1}, {4, 2})
-    refute Chess.valid_move?(game, {4, 1}, {4,0})
+    refute Chess.valid_move?(game, {4, 1}, {4, 0})
   end
 
   test "valid_move? should return true when try to move bishop in diagonal" do
-    game = Chess.new_game
+    game =
+      Chess.new_game()
       |> Chess.move({7, 2}, {4, 1})
 
     assert Chess.valid_move?(game, {4, 1}, {5, 0})
@@ -209,7 +215,8 @@ defmodule Chess.ChessTest do
   end
 
   test "valid_move? should return false when try to move bishop over another piece" do
-    game = Chess.new_game
+    game =
+      Chess.new_game()
       |> Chess.move({7, 2}, {2, 3})
 
     refute Chess.valid_move?(game, {2, 3}, {0, 5})
@@ -217,21 +224,22 @@ defmodule Chess.ChessTest do
   end
 
   test "valid_move? should return false when queen does a invalid move" do
-    refute Chess.new_game |> Chess.move({7, 3}, {4, 3})  |> Chess.valid_move?({4, 3}, {2, 2})
+    refute Chess.new_game() |> Chess.move({7, 3}, {4, 3}) |> Chess.valid_move?({4, 3}, {2, 2})
   end
 
-
   test "valid_move? should return true when queen moves diagonaly" do
-    game = Chess.new_game
-      |> Chess.move({7,3}, {4,3})
+    game =
+      Chess.new_game()
+      |> Chess.move({7, 3}, {4, 3})
 
     assert Chess.valid_move?(game, {4, 3}, {5, 4})
     assert Chess.valid_move?(game, {4, 3}, {3, 2})
   end
 
   test "valid_move? should return true when queen moves horizontaly" do
-    game = Chess.new_game
-      |> Chess.move({7,3},{4, 3})
+    game =
+      Chess.new_game()
+      |> Chess.move({7, 3}, {4, 3})
 
     assert Chess.valid_move?(game, {4, 3}, {5, 3})
     assert Chess.valid_move?(game, {4, 3}, {4, 4})
@@ -239,14 +247,16 @@ defmodule Chess.ChessTest do
   end
 
   test "valid_move? shoudl return false when queen moves over another piece" do
-     game = Chess.new_game
-      |> Chess.move({7,3},{4, 3})
+    game =
+      Chess.new_game()
+      |> Chess.move({7, 3}, {4, 3})
 
     refute Chess.valid_move?(game, {4, 3}, {0, 3})
   end
 
   test "valid_move? should return false when king moves more than one step" do
-    game = Chess.new_game
+    game =
+      Chess.new_game()
       |> Chess.move({7, 4}, {4, 4})
 
     refute Chess.valid_move?(game, {4, 4}, {2, 4})
@@ -256,7 +266,8 @@ defmodule Chess.ChessTest do
   end
 
   test "valid_move? should return true when king moves one tile horizontaly or vertically" do
-    game = Chess.new_game
+    game =
+      Chess.new_game()
       |> Chess.move({7, 4}, {4, 4})
 
     assert Chess.valid_move?(game, {4, 4}, {4, 3})
@@ -270,20 +281,20 @@ defmodule Chess.ChessTest do
   end
 
   test "valid_moves should return a set with all the possible moves at game start" do
-    moves = Chess.new_game |> Chess.valid_moves()
+    moves = Chess.new_game() |> Chess.valid_moves()
 
     assert moves == %{
-      {6, 0} => [{5, 0},{4, 0}],
-      {6, 1} => [{5, 1},{4, 1}],
-      {6, 2} => [{5, 2},{4, 2}],
-      {6, 3} => [{5, 3},{4, 3}],
-      {6, 4} => [{5, 4},{4, 4}],
-      {6, 5} => [{5, 5},{4, 5}],
-      {6, 6} => [{5, 6},{4, 6}],
-      {6, 7} => [{5, 7},{4, 7}],
-      {7, 1} => [{5, 2}, {5, 0}],
-      {7, 6} => [{5, 7}, {5, 5}]
-  }
+             {6, 0} => [{5, 0}, {4, 0}],
+             {6, 1} => [{5, 1}, {4, 1}],
+             {6, 2} => [{5, 2}, {4, 2}],
+             {6, 3} => [{5, 3}, {4, 3}],
+             {6, 4} => [{5, 4}, {4, 4}],
+             {6, 5} => [{5, 5}, {4, 5}],
+             {6, 6} => [{5, 6}, {4, 6}],
+             {6, 7} => [{5, 7}, {4, 7}],
+             {7, 1} => [{5, 2}, {5, 0}],
+             {7, 6} => [{5, 7}, {5, 5}]
+           }
   end
 
   test "check? should return false in the game start" do
@@ -292,8 +303,8 @@ defmodule Chess.ChessTest do
 
   test "check? should return true if one of the other team pieces can move into the current player king" do
     assert Chess.new_game()
-      |> Chess.move({6, 5}, {4, 5})
-      |> Chess.move({0, 3}, {4, 7})
-      |> Chess.check?()
+           |> Chess.move({6, 5}, {4, 5})
+           |> Chess.move({0, 3}, {4, 7})
+           |> Chess.check?()
   end
 end
